a5f31582428c54b150270ce2f43defe243d5bcee
rtezen23 <raul_tezen@hotmail.com>
2022-04-08 13:29:48 -0500

    avance portafolio2

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* MENU */
+
 document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
 
 let nav = document.getElementById("nav");
@@ -17,3 +19,25 @@ function ocultar_menu(){
 }
 
 background_menu.addEventListener("click", ocultar_menu);
+
+/* PORTAFOLIOS */
+const portafolios    = document.querySelector('.portafolio-elementos');
+const puntos     = document.querySelectorAll('.punto');
+
+
+puntos.forEach( ( element , i )=> {
+
+    puntos[i].addEventListener('click',()=>{
+        
+        let posicion  = i;
+        let operacion = posicion * -33;
+
+        portafolios.style.transform = `translateX(${ operacion }%)`
+
+        puntos.forEach( ( element , i )=>{
+            puntos[i].classList.remove('activo')
+        })
+
+        puntos[i].classList.add('activo')
+    })
+})
